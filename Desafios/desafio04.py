f9271bcd3d3e7388f97ecfc04069441270e83761
igorfariasoliveira <igor10farias@gmail.com>
2024-05-14 10:45:51 -0300

    Update desafio04.py

--- Desafios/desafio04.py
+++ Desafios/desafio04.py
@@ -2,4 +2,4 @@ objeto = input("Digite algo: ")
 print("O tipo primitivo é ",type(objeto))
 print("É um número? ",objeto.isnumeric())#Essa é outra forma de concaternar
 print("É uma letra ou palavra? " + str(objeto.isalpha()))
-print("É maiusculo? {}".format(objeto.isupper))
+print("É maiusculo? {}".format(objeto.isupper()))
