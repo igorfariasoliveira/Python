adfa6d8f2c66c4e72ede068f5a5fc182a88f7da5
igorfariasoliveira <igor10farias@gmail.com>
2024-05-14 11:40:20 -0300

    Create desafio07.py

---
+++ Desafios/desafio07.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite sua primeira nota: "))
+n2 = float(input("DIgite sua segunda nota: "))
+m = (n1+n2)/2
+print("Sua primeira nota foi {} e sua segunda nota foi {}.\nSua média é {}".format(n1,n2,m))
