6e1b118fb7b1922e21d3be9d7e5bf863bfed6738
igorfariasoliveira <igor10farias@gmail.com>
2024-05-15 10:40:39 -0300

    Update desafio11.py

--- Desafios/desafio11.py
+++ Desafios/desafio11.py
@@ -2,4 +2,4 @@ a = float(input("Qual a altura da parede em metros? "))
 l = float(input("Qual a largura da parede em metros? "))
 area = a*l
 quantidade = area/2
-print("A area dessa parede é {}m^2. \nConsiderando que cada litro de tinta pinta uma area de 2m^2, serão necessarios {} litros de tinta para pintar essa parede".format(area,quantidade))
+print("A area dessa parede é {}m². \nConsiderando que cada litro de tinta pinta uma area de 2m², serão necessarios {} litros de tinta para pintar essa parede".format(area,quantidade))
