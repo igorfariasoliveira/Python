d5145639093871586b00f4eba73285b5055b40c7
igorfariasoliveira <igor10farias@gmail.com>
2024-05-15 15:41:17 -0300

    Update desafio15.py

--- desafio15.py
+++ desafio15.py
@@ -3,4 +3,4 @@ quilomentros = float(input("Quantos quilometros rodou com ele? "))
 precoaluguel = dias * 60
 precodistancia = quilomentros * 0.15
 totalapagar = precoaluguel + precodistancia
-print('Você ira pagar o valor de {:.2f}R$'.format(totalapagar))
+print('Você ira pagar o valor de R${:.2f}'.format(totalapagar))
