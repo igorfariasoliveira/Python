a080d8c8d05ee7a8fa451a282d020524638b202c
igorfariasoliveira <igor10farias@gmail.com>
2024-05-17 15:03:13 -0300

    Update desafio22.py

--- Desafios/desafio22.py
+++ Desafios/desafio22.py
@@ -1,8 +1,10 @@
 nome = input("Qual o seu nome? ")
-nome_sem_espaço = nome.replace(" ","")
-nome_separado = nome.split(" ")
-primeiro_nome = nome_separado[0]
 print(nome.upper())
 print(nome.lower())
+
+nome_sem_espaço = nome.replace(" ","")
 print(len(nome_sem_espaço))
+
+nome_separado = nome.split(" ")
+primeiro_nome = nome_separado[0]
 print(len(primeiro_nome))
