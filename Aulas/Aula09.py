7823ad69ff269bae7eb9df48871f9f47ae15a444
igorfariasoliveira <igor10farias@gmail.com>
2024-05-17 10:38:46 -0300

    Update Aula09.py

--- Aulas/Aula09.py
+++ Aulas/Aula09.py
@@ -1,2 +1,6 @@
 frase = "Estou entediado"
-print(frase[6:15])
+teste = '   Sou flamenguista desde que nasci'
+print('dia' in frase)
+print(frase.replace('entediado','amando'))
+print(frase.upper())
+print(teste.strip())
