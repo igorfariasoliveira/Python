477dea560e2490e19607c2c005567fe31f076bbd
igorfariasoliveira <igor10farias@gmail.com>
2024-05-20 08:32:50 -0300

    Update desafio24.py

--- Desafios/desafio24.py
+++ Desafios/desafio24.py
@@ -1,5 +1,5 @@
-cidade = 'Santos'
+cidade = 'Terra dos Santos'
 cidade_separada = cidade.split(' ')
-print ('"Santos' in cidade_separada[0])
+print ('Santos' in cidade_separada[0])
 print(cidade)
 print(cidade_separada)
