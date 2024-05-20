4464a5866faedaac7b65164f41904282e5eb9a9e
igorfariasoliveira <igor10farias@gmail.com>
2024-05-20 14:49:10 -0300

    Update desafio24.py

--- Desafios/desafio24.py
+++ Desafios/desafio24.py
@@ -1,5 +1,9 @@
-cidade = 'SanTos da berra'
-cidade_separada = cidade.split(' ')
-print ('santos' in cidade_separada[0].lower())
-print(cidade)
-print(cidade_separada)
+#cidade = str('SanTo da berra').strip()
+#cidade_separada = cidade.split(' ')
+#print ('santo' in cidade_separada[0].lower())
+#print(cidade)
+#print(cidade_separada)
+
+#Forma reduzida:
+cidade =  str(input('Informe a cidade: ')).strip()
+print(cidade[:5].upper() == 'SANTOS')
