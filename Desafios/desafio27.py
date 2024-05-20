4331afcc9c0eafc9463af9fb8bdccf9b3cccd1c9
igorfariasoliveira <igor10farias@gmail.com>
2024-05-20 09:13:19 -0300

    Update desafio27.py

--- Desafios/desafio27.py
+++ Desafios/desafio27.py
@@ -1,1 +1,4 @@
 nome = input('Escreva seu nome completo: ')
+nome_separado = nome.split(' ')
+print("Seu primeiro nome é {}".format(nome_separado[0]))
+print("Seu ultimo nome é {}".format(nome_separado[-1]))
