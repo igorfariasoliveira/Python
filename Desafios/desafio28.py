7a593706e14b3ab0b92520d50afcdcb182812ab8
igorfariasoliveira <igor10farias@gmail.com>
2024-05-22 09:14:58 -0300

    Update desafio28.py

--- Desafios/desafio28.py
+++ Desafios/desafio28.py
@@ -2,3 +2,7 @@ import random
 numero_aleatorio = random.randint(0,5)
 num = int(input('Advinha o número que estou pensando entre 0 e 5... '))
 print('O numero que pensei foi {}'.format(numero_aleatorio))
+if numero_aleatorio == num:
+  print('Parabéns! Você acertou')
+else:
+  print('Que pena! Você errou')
