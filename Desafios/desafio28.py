5bd3685993561c3b99d81933a5f7d52f8630d54a
igorfariasoliveira <igor10farias@gmail.com>
2024-05-22 09:12:22 -0300

    Create desafio28.py

---
+++ Desafios/desafio28.py
@@ -0,0 +1,4 @@
+import random
+numero_aleatorio = random.randint(0,5)
+num = int(input('Advinha o número que estou pensando entre 0 e 5... '))
+print('O numero que pensei foi {}'.format(numero_aleatorio))
