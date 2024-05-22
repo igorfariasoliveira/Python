c08633a4ef18d0cccb097feebb71e5c8567f0fc0
igorfariasoliveira <igor10farias@gmail.com>
2024-05-22 11:14:47 -0300

    desafios

--- Desafios/desafio29.py
+++ Desafios/desafio29.py
@@ -2,6 +2,6 @@ vel = int(input('Em que velocidade você está? '))
 if vel > 80:
   multa = (vel - 80)*7
   print('Seu carro está acima da velocidade permitida!')
-  print('Terá que pagar uma multa de {}R$'.format(multa))
+  print('Terá que pagar uma multa de R${}'.format(multa))
 else:
   print("Você não está acima da velocidade")
--- Desafios/desafio31.py
+++ Desafios/desafio31.py
@@ -1,1 +1,8 @@
-dis = int(input(''))
+dis = int(input('Qual a distancia da viagem? '))
+if dis <= 200:
+  total = dis*0.50
+  print("Na sua viagem você irá pagar R${}".format(total))
+else:
+  total = dis*0.45
+  print("Na sua viagem você irá pagar R${}".format(total))
+  
