b93d79a4e9437b8b3616ce43ce72420850fe5e50
igorfariasoliveira <igor10farias@gmail.com>
2024-05-24 08:48:10 -0300

    Update desafio35.py

--- Desafios/desafio35.py
+++ Desafios/desafio35.py
@@ -4,4 +4,4 @@ c = int(input('Digite mais um lado do triângulo '))
 if abs(b - c)<a<b+c and abs(a-c)<b<a+c and abs(a-b)<c<a+b:
   print('\033[32mEssas retas podem formar um triangulo!')
 else:
-  print('\033Essas retas não podem formar um triangulo')
+  print('\033[31mEssas retas não podem formar um triangulo')
