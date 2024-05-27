8ae5d837769a210427edbf4e7251ad5853935443
igorfariasoliveira <igor10farias@gmail.com>
2024-05-27 14:17:54 -0300

    Update desafio38.py

--- Desafios/desafio38.py
+++ Desafios/desafio38.py
@@ -1,2 +1,7 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+  print('O número {} é maior que o número{}!'.format(n1,n2))
+elif n2 > n1:
+  
