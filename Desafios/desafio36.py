a965380bff28c1604f17711912cd46e4a154e5ff
igorfariasoliveira <igor10farias@gmail.com>
2024-05-27 10:34:15 -0300

    Update desafio36.py

--- Desafios/desafio36.py
+++ Desafios/desafio36.py
@@ -3,6 +3,8 @@ salario = float(input('Quanto você recebe de salário? '))
 tempo = int(input('Em quantos anos você deseja quitar essa compra? '))
 parcelas = valor/(tempo*12)
 credito = salario*0.30
-if credito <= parcelas:
+if credito >= parcelas:
   print('Parabéns, sua compra foi aprovada. Você pagará parcelas de {:.2f}'.format(parcelas))
-else
+else:
+  print('Infelizmente as parcelas ficaram muito altas para você pagar. Tente novamente com um prazo maior.')
+  
