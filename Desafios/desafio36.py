2ea2b00bf3353abd8607e81eda2a1c911e08949d
igorfariasoliveira <igor10farias@gmail.com>
2024-05-27 09:03:50 -0300

    Update desafio36.py

--- Desafios/desafio36.py
+++ Desafios/desafio36.py
@@ -2,4 +2,7 @@ valor = int(input('Qual o valor do imóvel? '))
 salario = float(input('Quanto você recebe de salário? '))
 tempo = int(input('Em quantos anos você deseja quitar essa compra? '))
 parcelas = valor/(tempo*12)
-print(parcelas)
+credito = salario*0.30
+if credito <= parcelas:
+  print('Parabéns, sua compra foi aprovada. Você pagará parcelas de {:.2f}'.format(parcelas))
+else
