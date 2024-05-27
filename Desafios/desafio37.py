79312a9c33678543012217b1129ab8c9ed9bbc33
igorfariasoliveira <igor10farias@gmail.com>
2024-05-27 11:01:52 -0300

    Update desafio37.py

--- Desafios/desafio37.py
+++ Desafios/desafio37.py
@@ -1,2 +1,5 @@
-num = int(input('DIgite um número: '))
-base = int(input('Você quer em \n 1: Binario\n 2: Octal\n 3: Hexadecimal'))
+num = int(input('Digite um número: '))
+base = int(input('Você quer em \n 1: Binario\n 2: Octal\n 3: Hexadecimal\nDigite sua escolha: '))
+if base == 1:
+  binario = bin(num)[2:]
+  print('O número {} na base binaria é {}'.format(num,binario))
