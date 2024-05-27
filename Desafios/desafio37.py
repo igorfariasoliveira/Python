8f8c2c6adf367a8de2a62578db7a565145c66803
igorfariasoliveira <igor10farias@gmail.com>
2024-05-27 11:12:33 -0300

    Update desafio37.py

--- Desafios/desafio37.py
+++ Desafios/desafio37.py
@@ -3,3 +3,11 @@ base = int(input('Você quer em \n 1: Binario\n 2: Octal\n 3: Hexadecimal\nDigit
 if base == 1:
   binario = bin(num)[2:]
   print('O número {} na base binaria é {}'.format(num,binario))
+elif base == 2:
+  octal = oct(num)[2:]
+  print('O número {} na base octal é {}'.format(num,octal))
+elif base == 3:
+  hexadecimal = hex(num)[2:]
+  print('O número {} na base hexadecimal é "{}"'.format(num,hexadecimal))
+else:
+  print('Escolha um número entre as opções acima')
