d0826be447f018db334ca15b304cdb504325df50
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 13:33:16 -0300

    Update desafio45.py

--- Desafios/desafio45.py
+++ Desafios/desafio45.py
@@ -1,4 +1,11 @@
 from random import choice
 print('Vamos jogar pedra, papel e tesoura?')
-jogador = str(input('Qual você vai jogar? ')).lower
-computador = ['pedra','papel','tesoura']
+jogador = str(input('Qual você vai jogar? ')).lower()
+nomes = ['pedra','papel','tesoura']
+computador = choice(nomes)
+if computador == jogador:
+  print('Eu tambem escolhi {}. Deu empate'.format(computador))
+elif (jogador == 'pedra' and computador == 'papel') or (jogador == 'tesoura' and computador == 'pedra') or (jogador == 'papel' and computador == 'tesoura'):
+  print('Eu escolhi {}. Eu ganhei!'.format(computador))
+else:
+  print('Eu escolhi {}. Parabéns, você ganhou!'.format(computador))
