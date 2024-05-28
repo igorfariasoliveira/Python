8146d91e9c9ebcae4bfbb9e28d4d41d444e68db0
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 08:48:52 -0300

    Update desafio42.py

--- Desafios/desafio42.py
+++ Desafios/desafio42.py
@@ -3,5 +3,11 @@ b = int(input('Digite outro lado do triângulo '))
 c = int(input('Digite mais um lado do triângulo '))
 if abs(b - c)<a<b+c and abs(a-c)<b<a+c and abs(a-b)<c<a+b:
   print('\033[32mEssas retas podem formar um triangulo!')
+  if a == b == c:
+    print('É um triângulo equilátero')
+  elif a != b != c:
+    print('É um triângulo escaleno')
+  else:
+    print('É um triângulo isoceles')
 else:
   print('\033[31mEssas retas não podem formar um triangulo!')
