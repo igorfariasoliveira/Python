e69d6398d735257f57374c2eb82c3edd744ee683
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 08:16:22 -0300

    Create desafio41.py

---
+++ Desafios/desafio41.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+ano_atual = datetime.now().year
+nasc = int(input('Em que ano você nasceu? '))
+idade = ano_atual-nasc
+if idade <= 9:
+  print('Você tem {} anos. Esta na categoria mirim'.format(idade))
+elif idade > 9 and idade <= 14:
+  print('Você tem {} anos. Esta na categoria infantil'.format(idade))
+elif idade > 14 and idade <= 19:
+  print('Você tem {} anos. Esta na categoria junior'.format(idade))
