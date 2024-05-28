37627a7ec77badbfc99e910945284fea2b7bde8d
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 10:53:42 -0300

    Update desafio44.py

--- Desafios/desafio44.py
+++ Desafios/desafio44.py
@@ -4,4 +4,4 @@ if opcao == 1:
   total = preco-(preco*0.10)
   print('Com o desconto de 10% você ira pagar R${:.2f}'.format(total))
 elif opcao == 2:
-  plano = int(input('Escolha o plano de pagamento:\n'))
+  plano = int(input('Escolha o plano de pagamento:\n1: A vista\n2: 2x n'))
