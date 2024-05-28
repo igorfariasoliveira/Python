87fc944fd7a1bd1747312093024693a4b4264830
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 10:37:28 -0300

    Create desafio44.py

---
+++ Desafios/desafio44.py
@@ -0,0 +1,7 @@
+preco = float(input('Qual o valor do produto? '))
+opcao = int(input("Selecione a opçao de pagamento:\n1: dinheiro ou cheque\n2: cartão de credito\n"))
+if opcao == 1:
+  total = preco-(preco*0.10)
+  print('Com o desconto de 10% você ira pagar R${:.2f}'.format(total))
+elif opcao == 2:
+  plano = int(input('Escolha o plano de pagamento:\n'))
