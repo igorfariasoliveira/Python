640c07ee15628baceb8a7caa0c61fb87086145fe
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 08:07:24 -0300

    Update desafio40.py

--- Desafios/desafio40.py
+++ Desafios/desafio40.py
@@ -1,3 +1,9 @@
 n1 = float(input('Qual foi a sua primeira nota? '))
 n2 = float(input('Qual foi a sua segunda nota? '))
 media = (n1+n2)/2
+if media < 5:
+  print('Sua média foi {:.1f}. Você está reprovado!'.format(media))
+elif media >= 5 and media < 6.9:
+  print('Sua média foi {:.1f}. Você irá fazer a recuperação!'.format(media))
+else:
+  print('Sua média foi {:.1f}. Parabéns, você foi aprovado!'.format(media))
