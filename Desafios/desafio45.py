b835002a4abb6b8b9c4093861ec01d3ec9dfe67b
igorfariasoliveira <igor10farias@gmail.com>
2024-05-28 13:35:17 -0300

    Update desafio45.py

--- Desafios/desafio45.py
+++ Desafios/desafio45.py
@@ -7,5 +7,7 @@ if computador == jogador:
   print('Eu tambem escolhi {}. Deu empate'.format(computador))
 elif (jogador == 'pedra' and computador == 'papel') or (jogador == 'tesoura' and computador == 'pedra') or (jogador == 'papel' and computador == 'tesoura'):
   print('Eu escolhi {}. Eu ganhei!'.format(computador))
-else:
+elif (jogador == 'pedra' and computador == 'tesoura') or (jogador == 'tesoura' and computador == 'papel') or (jogador == 'papel' and computador == 'pedra'):
   print('Eu escolhi {}. Parabéns, você ganhou!'.format(computador))
+else:
+  print('Você não escolheu pedra, papel ou tesoura. Tente novamente!')
