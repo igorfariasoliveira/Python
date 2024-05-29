74a0a59c29b5f2f4564d1ffcb6ac816ef180ffb1
igorfariasoliveira <igor10farias@gmail.com>
2024-05-29 13:28:34 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,9 @@ tipo = str(input('Escolha qual o tipo do pokêmon inicial você vai escolher ent
 if tipo == 'água' or tipo == 'agua':
   pokemon = int(input('Você deve escolher entre:\n1: Squirtle\n2: Totodile\n3: Mudkip\nDigite o número do pokemon correspondente: '))
   if pokemon == 1:
-    print('Parabéns! Você escolheu o pokemon Squirtle da região de Kanto. Boa sorte na sua jornada ')
+    print('Parabéns! Você escolheu o pokemon Squirtle da região de Kanto. Boa sorte na sua jornada! ')
+  elif pokemon == 2:
+    print('Parabéns! Você escolheu o pokemon Totodile da região de Johto. Boa sorte na sua jornada! ')
+  elif pokemon == 3:
+    print('Parabéns! Você escolheu o pokemon Mudkip da região de Hoenn. Boa sorte na sua jornada! ')
+elif
