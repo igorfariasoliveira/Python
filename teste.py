feae294db7e7492ce6e7cc77413cf4b462ac6631
igorfariasoliveira <igor10farias@gmail.com>
2024-05-29 13:19:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,3 @@
-tipo = str(input('Escolha qual o tipo do pokêmon inicial você vai escolher entre água, fogo e planta '))
+tipo = str(input('Escolha qual o tipo do pokêmon inicial você vai escolher entre água, fogo e planta ')).lower()
+if tipo == 'água' or tipo == 'agua':
+  pokemon = int()
