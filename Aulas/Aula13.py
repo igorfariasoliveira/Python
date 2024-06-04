96fcff8d481c94263e8f864270e62c4e9a110322
igorfariasoliveira <igor10farias@gmail.com>
2024-06-04 10:30:31 -0300

    Update Aula13.py

--- Aulas/Aula13.py
+++ Aulas/Aula13.py
@@ -1,3 +1,5 @@
-n = int(input('Digite um número do contador: '))
-for c in range(0,n+1):
+i = int(input('Em qual número você quer começar a contagem? '))
+f = int(input('Em qual número quer terminar a contagem? '))
+p = int(input('Pulando quantos números? '))
+for c in range(i,f,p):
   print(c)
