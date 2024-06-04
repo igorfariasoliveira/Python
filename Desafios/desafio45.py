bc62f913b93edb6c918f66edb80a4cbb1b2375d1
igorfariasoliveira <igor10farias@gmail.com>
2024-06-04 07:31:34 -0300

    Update desafio45.py

--- Desafios/desafio45.py
+++ Desafios/desafio45.py
@@ -4,9 +4,13 @@ jogador = str(input('Qual você vai jogar? ')).lower()
 nomes = ['pedra','papel','tesoura']
 computador = choice(nomes)
 if computador == jogador:
+  print('-=' * 21)
   print('Eu tambem escolhi {}. Deu empate'.format(computador))
+  print('-=' * 21)
 elif (jogador == 'pedra' and computador == 'papel') or (jogador == 'tesoura' and computador == 'pedra') or (jogador == 'papel' and computador == 'tesoura'):
+  print('-=' * 21)
   print('Eu escolhi {}. Eu ganhei!'.format(computador))
+  print('-=' * 21)
 elif (jogador == 'pedra' and computador == 'tesoura') or (jogador == 'tesoura' and computador == 'papel') or (jogador == 'papel' and computador == 'pedra'):
   print('-=' * 21)
   print('Eu escolhi {}. Parabéns, você ganhou!'.format(computador))
