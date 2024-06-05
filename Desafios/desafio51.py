ede6ac3035ccaa885e1649ce0406ac931fcd13da
igorfariasoliveira <igor10farias@gmail.com>
2024-06-05 08:20:21 -0300

    Update desafio51.py

--- Desafios/desafio51.py
+++ Desafios/desafio51.py
@@ -1,4 +1,6 @@
 primeiro = int(input('Informe o primeiro termo da PA: '))
 razao = int(input('Informe a razão da PA: '))
-for c in range (primeiro,10,razao):
-  print(c)
+
+for c in range (0,10):
+  res = primeiro + c*razao
+  print(res)
