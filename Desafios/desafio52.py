0b3b9856bd3496d27ab4bf64f89480a6a6a0654f
igorfariasoliveira <igor10farias@gmail.com>
2024-06-05 08:34:30 -0300

    Update desafio52.py

--- Desafios/desafio52.py
+++ Desafios/desafio52.py
@@ -4,4 +4,6 @@ for c in range(1,n+1):
   if n % c == 0:
     res+=1
 if res == 2:
-  print('Esse número é primo')
+  print('O número {} é primo!'.format(n))
+else:
+  print('O numero {} não é primo!'.format(n))
