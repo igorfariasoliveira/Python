736a27ebe9d5038e909a62fdc17df0655ef27ce8
igorfariasoliveira <igor10farias@gmail.com>
2024-06-05 08:41:26 -0300

    desa

--- Desafios/desafio52.py
+++ Desafios/desafio52.py
@@ -1,9 +1,12 @@
 n = int(input('Digite um número primo: '))
-res = 0
-for c in range(1,n+1):
-  if n % c == 0:
-    res+=1
-if res == 2:
-  print('O número {} é primo!'.format(n))
+if n < 2:
+    print('O número {} não é primo!'.format(n))
 else:
-  print('O numero {} não é primo!'.format(n))
+  res = 0
+  for c in range(1,n+1):
+    if n % c == 0:
+      res+=1
+  if res == 2:
+    print('O número {} é primo!'.format(n))
+  else:
+    print('O numero {} não é primo!'.format(n))
