fb5becdeb74aa77f747de047886006b711b36ea5
igorfariasoliveira <igor10farias@gmail.com>
2024-06-05 08:59:41 -0300

    Update desafio54.py

--- Desafios/desafio54.py
+++ Desafios/desafio54.py
@@ -2,3 +2,8 @@ maior = 0
 menor = 0
 for c in range(0,7):
   idade= int(input('Qual a sua idade? '))
+  if idade >= 18:
+    maior+=1
+  else:
+    menor+=1
+print('Das sete pessoas informadas {} são menores de idade e {} são maiores de idade!'.format(menor,maior))
