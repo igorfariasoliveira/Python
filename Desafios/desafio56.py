e17f7cff41d8f035e959f188aa558e2d413640a7
igorfariasoliveira <igor10farias@gmail.com>
2024-06-06 16:13:23 -0300

    Update desafio56.py

--- Desafios/desafio56.py
+++ Desafios/desafio56.py
@@ -7,12 +7,13 @@ for c in range(0,4):
   idade = int(input('Qual a sua idade? '))
   sexo = int(input('Se seu sexo for masculino digite 1. Se for feminino digite 2. '))
   idade_grupo += idade
-  if mais_velho is None or idade > mais_velho:
+  if mais_velho is None or idade > mais_velho and sexo==1:
      mais_velho = idade
      nome_velho = nome
-  if sexo == 2:
+  if sexo == 2 and idade < 20:
     contador_fem+=1
 media = idade_grupo/4
-print(media)
-print(nome_velho)
+print('A media de idade desse grupo de pessoas é {} anos!'.format(media))
+print('O homem mais velho é o {} e tem {} anos!'.format(nome_velho,mais_velho))
+print('A quantidade de mulheres com menos de 20 anos é {}!'.format(contador_fem))
 
