c139c8368dc142c676956ccfb24196339682fb3a
igorfariasoliveira <igor10farias@gmail.com>
2024-06-06 16:05:41 -0300

    Update desafio56.py

--- Desafios/desafio56.py
+++ Desafios/desafio56.py
@@ -4,6 +4,7 @@ nome_velho = None
 for c in range(0,4):
   nome = str(input('Qual o seu nome? '))
   idade = int(input('Qual a sua idade? '))
+  sexo = int(input('Se seu sexo for masculino digite 1. Se for feminino digite 2. '))
   idade_grupo += idade
   if mais_velho is None or idade > mais_velho:
      mais_velho = idade
