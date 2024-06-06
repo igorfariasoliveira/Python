fcb8a6a6c46d2b5ad56f59e54e26fd21c2150203
igorfariasoliveira <igor10farias@gmail.com>
2024-06-06 16:06:33 -0300

    Update desafio56.py

--- Desafios/desafio56.py
+++ Desafios/desafio56.py
@@ -1,6 +1,7 @@
 idade_grupo = 0
 mais_velho = None
 nome_velho = None
+contador_fem = 0
 for c in range(0,4):
   nome = str(input('Qual o seu nome? '))
   idade = int(input('Qual a sua idade? '))
@@ -9,6 +10,9 @@ for c in range(0,4):
   if mais_velho is None or idade > mais_velho:
      mais_velho = idade
      nome_velho = nome
+  if sexo == 2:
+    contador_fem+=1
 media = idade_grupo/4
 print(media)
 print(nome_velho)
+
