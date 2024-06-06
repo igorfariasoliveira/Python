539f02eb70dbc9efd5a9961f3fff428917ba2e76
igorfariasoliveira <igor10farias@gmail.com>
2024-06-06 15:27:17 -0300

    Update desafio55.py

--- Desafios/desafio55.py
+++ Desafios/desafio55.py
@@ -1,8 +1,8 @@
-maior = 0
+maior = none
 menor = None
 for c in range(0,5):
   peso = float(input('Qual o seu peso?'))
-  if peso > maior:
+  if maior is None or peso > maior:
     maior = peso
   if menor is None or peso < menor:
     menor = peso
