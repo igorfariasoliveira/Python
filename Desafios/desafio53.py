b04fe0a6238bfd2281f6e243f5830f3b8bd52e65
igorfariasoliveira <igor10farias@gmail.com>
2024-06-11 08:05:14 -0300

    Update desafio53.py

--- Desafios/desafio53.py
+++ Desafios/desafio53.py
@@ -1,4 +1,8 @@
-frase = str(input("Escreva uma pequena frase: ")).replace(' ','')
-
-
-print(frase)
+frase = str(input("Escreva uma pequena frase: ")).replace(' ','').upper()
+inverso = ''
+for letra in range(len(frase) -1, -1, -1):
+  inverso += frase[letra]
+if inverso == frase:
+  print('Essa frase é um palindromo!')
+else:
+  print('Essa frase não é um palindromo')
