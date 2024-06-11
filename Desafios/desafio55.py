e547e83140b5b7e8e377e2e517243498dd5e2292
igorfariasoliveira <igor10farias@gmail.com>
2024-06-11 08:26:40 -0300

    Update desafio55.py

--- Desafios/desafio55.py
+++ Desafios/desafio55.py
@@ -1,9 +1,9 @@
 maior = None
 menor = None
-for c in range(0,5):
-  peso = float(input('Qual o seu peso?'))
+for c in range(1,6):
+  peso = float(input('Peso da {}ª pessoa: '.format(c)))
   if maior is None or peso > maior:
     maior = peso
   if menor is None or peso < menor:
     menor = peso
-print('O maior peso é {:.2f}kg e o menor é {:.2f}kg'.format(maior,menor))
+print('O maior peso é {:.1f}kg e o menor é {:.1f}kg'.format(maior,menor))
