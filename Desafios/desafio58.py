4ef2012c10cf2f4cb56d334b1b18da74f1a381c8
igorfariasoliveira <igor10farias@gmail.com>
2024-06-12 13:17:08 -0300

    Update desafio58.py

--- Desafios/desafio58.py
+++ Desafios/desafio58.py
@@ -1,8 +1,11 @@
 from random import randint
 numero_aleatorio = None
-num = None
+num = 0
+contador = 0
 while numero_aleatorio != num:
   numero_aleatorio = randint(0,5)
   num = int(input('Advinha o número que estou pensando entre 0 e 5... '))
+  contador += 1
   if numero_aleatorio != num:
-    print('Eu pensei no número {}. Você errou, mas pode tentar novamente!')
+    print('Eu pensei no número {}. Você errou, mas pode tentar novamente!'.format(numero_aleatorio))
+print('Parabéns, você acertou em {} jogadas!'.format(contador)) 
