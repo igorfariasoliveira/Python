7e2b83c6c7ee1e2511cdeab35ba8e16706f7a6f6
igorfariasoliveira <igor10farias@gmail.com>
2024-06-18 10:35:38 -0300

    Update desafio59.py

--- Desafios/desafio59.py
+++ Desafios/desafio59.py
@@ -4,4 +4,17 @@ escolha = None
 
 while escolha != 5:
   escolha = int(input('Escolha uma das opções abaixo:\n[1] Somar\n[2] Mulriplicar\n[3] Maior\n[4] Novos números\n[5] Sair do programa\n'))
-  
+  if escolha == 1:
+    soma = n1+n2
+    print('\033[92mA soma dos números {} e {} é {}\033[0m'.format(n1, n2, soma))
+  elif escolha == 2:
+    mult = n1*n2
+    print('\033[92mA multiplicação dos números {} e {} é {}\033[0m'.format(n1,n2,mult))
+  elif escolha == 3:
+    if n1>n2:
+      print('O número maios é {}'.format(n1))
+    else:
+      print('O número maios é {}'.format(n2))
+  elif escolha == 4:
+    print('Teste')
+  else:
