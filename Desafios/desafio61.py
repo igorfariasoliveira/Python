5d5a3b02f58f30b609b104f232b3608a4990de9b
igorfariasoliveira <igor10farias@gmail.com>
2024-06-18 13:56:33 -0300

    desafios

--- Desafios/desafio61.py
+++ Desafios/desafio61.py
@@ -2,7 +2,10 @@ primeiro = int(input('Informe o primeiro termo da PA: '))
 razao = int(input('Informe a razão da PA: '))
 c = 0
 res = 0
-while c < 10:
+esc = None
+while c < 10 and esc!= 0:
   res = primeiro + c*razao
   c+=1
   print(res)
+   
+      
---
+++ Desafios/desafio62.py
@@ -0,0 +1,4 @@
+num= int(input("Digite quantos elementos da sequencia de Fibonacci você quer ver: "))
+c = 0
+while c <= num:
+  
