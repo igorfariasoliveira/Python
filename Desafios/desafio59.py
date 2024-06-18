6f518572f6632ca5ca342586e8c8561da8112a37
igorfariasoliveira <igor10farias@gmail.com>
2024-06-18 10:15:59 -0300

    Update desafio59.py

--- Desafios/desafio59.py
+++ Desafios/desafio59.py
@@ -3,5 +3,5 @@ n2 = int(input('Digite outro número: '))
 escolha = None
 
 while escolha != 5:
-  escolha = int(input('Escolha uma das opções abaixo:\n[1] Somar\n'))
+  escolha = int(input('Escolha uma das opções abaixo:\n[1] Somar\n[2] Mulriplicar\n[3] Maior\n[4] Novos números\n[5] Sair do programa\n'))
   
