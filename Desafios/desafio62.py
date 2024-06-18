2b9042ffe69783d0af2123862c954c008512a079
igorfariasoliveira <igor10farias@gmail.com>
2024-06-18 14:19:48 -0300

    Update desafio62.py

--- Desafios/desafio62.py
+++ Desafios/desafio62.py
@@ -1,4 +1,11 @@
 num= int(input("Digite quantos elementos da sequencia de Fibonacci você quer ver: "))
-c = 0
+c = 1
+na = 1
+nb = 0
+nc = 0
 while c <= num:
-  
+  print(na)
+  nb = na 
+  na += nc
+  nc = nb
+  c+=1
