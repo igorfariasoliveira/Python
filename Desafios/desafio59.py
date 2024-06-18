0946f3b9c65b092164c1c61b60ee887dfab51f07
igorfariasoliveira <igor10farias@gmail.com>
2024-06-18 10:12:50 -0300

    Update desafio59.py

--- Desafios/desafio59.py
+++ Desafios/desafio59.py
@@ -1,4 +1,7 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
+escolha = None
 
-while
+while escolha != 5:
+  escolha = int(input('Escolha uma das opções abaixo:\n[1] Somar\n'))
+  
