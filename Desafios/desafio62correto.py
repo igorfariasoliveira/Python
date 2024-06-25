bb4f5b096b8fd57072e986394bed15c23b248cce
igorfariasoliveira <igor10farias@gmail.com>
2024-06-25 09:33:38 -0300

    Update desafio62correto.py

--- Desafios/desafio62correto.py
+++ Desafios/desafio62correto.py
@@ -3,8 +3,10 @@ razao = int(input('Informe a razão da PA: '))
 c = 0
 res = 0
 esc = None
-while c < 10 and esc!= 0:
-  res = primeiro + c*razao
-  c+=1
-  print('{} -> '.format(res), end='')
+confirma = ''
+while confirma not in 'não':
+  while c < 10 and esc!= 0:
+    res = primeiro + c*razao
+    c+=1
+    print('{} -> '.format(res), end='')
    
