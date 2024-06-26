efbbf284decc3828174bcdac2faffedc4df18125
igorfariasoliveira <igor10farias@gmail.com>
2024-06-26 08:54:46 -0300

    Update desafio67.py

--- Desafios/desafio67.py
+++ Desafios/desafio67.py
@@ -1,1 +1,10 @@
-
+c = 1
+num = 0
+while True:
+  num = int(input('Qual o valor você deseja ver a tabuada? '))
+  while c <=10:
+    print(f"{num} x {c} = {num*c}")
+    c+=1
+  c=1
+  if num < 0:
+    break
