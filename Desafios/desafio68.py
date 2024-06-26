22ad91fb9173f5b24d5926b7c0b862059b9051d2
igorfariasoliveira <igor10farias@gmail.com>
2024-06-26 11:16:57 -0300

    Update desafio68.py

--- Desafios/desafio68.py
+++ Desafios/desafio68.py
@@ -1,9 +1,13 @@
 from random import randint
 
 while True:
+  print('-'*20)
   jog = int(input('Escolha um número: '))
-  comp = randint(0,99)
+  print('-'*20)
+  comp = randint(1,99)
   esco = str(input('Você quer par ou impar?(P/I) ')).lower()
   soma = comp + jog
   if soma % 2 == 0:
-    
+    if esco == 'p':
+      print(f'Você escolheu {jog} e eu {comp}, somando dá {soma} que é um número par')
+      print('Parabéns, você venceu!')
