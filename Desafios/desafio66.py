471eeaf7a109e12a7572b1187c8a2eb532c982bc
igorfariasoliveira <igor10farias@gmail.com>
2024-06-26 08:47:44 -0300

    Update desafio66.py

--- Desafios/desafio66.py
+++ Desafios/desafio66.py
@@ -2,7 +2,7 @@ n = c = soma= 0
 while True:
   n = int(input('Digite um número (999 para parar e somar): '))
   c+=1
-  soma += n
   if n == 999:
     break
-print(f"Você digitou {c} números. A soma ")
+  soma += n
+print(f"Você digitou {c} números. A soma é {soma}")
