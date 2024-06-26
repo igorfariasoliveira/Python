db52c75dd8d44877e6081fb3984b3e1691f6657a
igorfariasoliveira <igor10farias@gmail.com>
2024-06-26 11:14:20 -0300

    Update desafio68.py

--- Desafios/desafio68.py
+++ Desafios/desafio68.py
@@ -4,3 +4,6 @@ while True:
   jog = int(input('Escolha um número: '))
   comp = randint(0,99)
   esco = str(input('Você quer par ou impar?(P/I) ')).lower()
+  soma = comp + jog
+  if soma % 2 == 0:
+    
