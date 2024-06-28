02fe2c792547f4748c7814c93e9968c4f47ea8e0
igorfariasoliveira <igor10farias@gmail.com>
2024-06-28 08:04:33 -0300

    Update deasfio69.py

--- Desafios/deasfio69.py
+++ Desafios/deasfio69.py
@@ -4,12 +4,13 @@ while True:
   print('-'*20)
   print('CADASTRO DE PESSOA')
   print('-'*20)
-  idade = int('Idade: ')
-  sexo = str(input('Sexo(M/F): ')).lower
+  idade = int(input('Idade: '))
+  sexo = str(input('Sexo(M/F): ')).lower()
   if idade > 18:
     maiorIdade+=1
   if sexo == 'm':
     homens+=1
   if sexo == 'f' and idade <20:
     mulherMenor+=1
+  print(maiorIdade,homens,mulherMenor)
   
